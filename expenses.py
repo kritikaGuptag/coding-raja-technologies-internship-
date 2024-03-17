6fb1e53e12445aad15ea29c04685359ce817fad1
kritikaGuptag <163744887+kritikaGuptag@users.noreply.github.com>
2024-03-17 19:42:27 +0530

    Add files via upload

---
+++ expenses.py
@@ -0,0 +1,19 @@
+from token import AMPER
+from typing import Self
+from unicodedata import category
+
+
+class Expense:
+    class amount:
+     def __init__(Self,__name__,category,amount) -None:# type: ignore
+      Self.name= __name__
+      Self.category= category
+      Self.amount=amount
+
+    def __repr__(self) -> str:
+        return f"<Expense: {self.name},{self.category},${self.amount:.2f}>"
+
+
+
+    
+
